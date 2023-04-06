e9be1c41752afba7c06d007ee7efcbb400f90b02
Adam <rogowiak333@gmail.com>
2023-04-06 23:49:36 +0200

    Create index.php
    Added form
    To do: form validation, tidy up the code

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>Rejestracja</header>
+    <main>
+
+		<form action = "zapisani.php" method = "post">
+			Imię i Nazwisko: <input type = "text" name = "name1">
+			<br>
+			Wk: <input type = "number" name = "WK1">
+			<br>
+			PID: <input type = "number" name = "PID1">
+			<br>
+			Imię i Nazwisko: <input type = "text" name = "name2">
+			<br>
+			Wk: <input type = "number" name = "WK2">
+			<br>
+			PID: <input type = "number" name = "PID2">
+			<br>
+			<button type="submit" value="Submit">Zapisz</button>
+		</form>
+
+    </main> 
+</body>
+</html>
