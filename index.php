3412c195a89f3c99e262c4be3bf8426c9ea8f78e
Adam <rogowiak333@gmail.com>
2023-04-20 11:27:15 +0200

    Styles and form validation
    Added form validation and styles,
    still have some styling to do

--- index.php
+++ index.php
@@ -1,31 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
+	<link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rejestracja</title>
+	<link rel="stylesheet" href="styles.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>Rejestracja</header>
+    <header>
+		<div class="title">Rejestracja</div>
+		</header>
     <main>
-
 		<form action = "zapisani.php" method = "post">
-			Imię i Nazwisko: <input type = "text" name = "name1">
-			<br>
-			Wk: <input type = "number" name = "WK1">
-			<br>
-			PID: <input type = "number" name = "PID1">
-			<br>
-			Imię i Nazwisko: <input type = "text" name = "name2">
-			<br>
-			Wk: <input type = "number" name = "WK2">
-			<br>
-			PID: <input type = "number" name = "PID2">
-			<br>
-			<button type="submit" value="Submit">Zapisz</button>
+			<div class="player1">
+			<div class="name">Zawodnik #1</div>
+			Imię i Nazwisko: 
+			<input type = "text" name = "name1">
+			WK: 
+			<input list = "WK" name = "WK1">
+			<datalist id="WK">
+				<option value=0>
+				<option value=0.5>
+				<option value=1>
+				<option value=1.5>
+				<option value=2>
+				<option value=2.5>
+				<option value=3>
+				<option value=4>
+				<option value=5>
+				<option value=7>
+				<option value=9>
+				<option value=11>
+				<option value=12>
+				<option value=13>
+				<option value=15>
+				<option value=17>
+				<option value=18>
+				<option value=19>
+				<option value=21>
+				<option value=24>
+			  </datalist>
+			PID: 
+			<input type = "text" name = "PID1" maxlength="5">
+			</div>
+			<div class="player2">
+			<div class="name">Zawodnik #2</div>
+			Imię i Nazwisko: 
+			<input type = "text" name = "name2">
+			WK: 
+			<input list = "WK" name = "WK2">
+			PID: 
+			<input type = "text" name = "PID2" maxlength="5">
+			</div>
+			<button class = "saveButton" type="submit" value="Submit">Zapisz</button>
 		</form>
-
     </main> 
 </body>
 </html>
--- zapisani.php
+++ zapisani.php
@@ -1,34 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rejestracja</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>Lista startowa</header>
-    <main>
+    <header><div class="title">Rejestracja</div></header>
+    <main id = "zapisani" >
         <?php 
-        $conn = new mysqli('localhost', 'root', '', 'zapisani'); //The Blank string is the password        
-        /* Check connection
-        if ($conn->connect_error) {
+        $conn = new mysqli('localhost', 'root', '', 'zapisani');   
+        /*if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
         else{
             echo "Connected successfully";
         }
         */
+        
 
-        $query = "SELECT Name1, WK1, Name2, WK2 FROM zapisani"; //You don't need a ; like you do in SQL
-        $result = mysqli_query($conn, $query);
-        echo "<table>"; // start a table tag in the HTML
+        $Name1 = $_POST['name1'];
+		$WK1 = $_POST['WK1'];
+		$PID1 = $_POST['PID1'];
+		$Name2 = $_POST['name2'];
+		$WK2 = $_POST['WK2'];
+		$PID2 = $_POST['PID2'];
+
+
+            $query = "SELECT * FROM zapisani WHERE PID1 = ? OR PID2 = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ii", $PID1, $PID2);
+            $stmt->execute();
+            $res = $stmt->get_result();
+
+            $query2 = "SELECT * FROM zapisani WHERE PID1 = ? OR PID2 = ?";
+            $stmt2 = $conn->prepare($query2);
+            $stmt2->bind_param("ii", $PID2, $PID1);
+            $stmt2->execute();
+            $res2 = $stmt2->get_result();
+
+            if ($PID1 != 0 && $PID2 != 0) {
+                if($res -> num_rows > 0 || $res2 -> num_rows > 0)
+                {echo "<div class='box'><div class='alert'>Co najmniej jeden z zawodników jest już zapisany</div>
+                    <div class='alert2'>zweryfikuj wprowadzone dane</div>
+                    <div class='links'><a href = 'index.php'> Powrót do zapisów </a>
+                    <a href = 'lista.php'>Lista startowa</a></div></div>
+                    ";}
+                
+                else{
+                    {
+                        $sql = "INSERT INTO zapisani (Name1,PID1, WK1, Name2, PID2, WK2) VALUES ('$Name1','$PID1','$WK1','$Name2','$PID2','$WK2')";
+                        mysqli_query($conn, $sql);
+                        $query = "SELECT Name1, WK1, Name2, WK2 FROM zapisani"; //You don't need a ; like you do in SQL
+                        $result = mysqli_query($conn, $query);
+                        echo "<table>"; // start a table tag in the HTML
+                        while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+                        echo "<tr><td>" . htmlspecialchars($row['Name1']) . "</td><td>" . htmlspecialchars($row['WK1']) . "</td><td>" . htmlspecialchars($row['Name2']) . "</td><td>" . htmlspecialchars($row['WK2']) . "</td></tr>";  //$row['index'] the index here is a field name
+                        }
+                    }
+                }
+            }
+            else{
+            $sql = "INSERT INTO zapisani (Name1,PID1, WK1, Name2, PID2, WK2) VALUES ('$Name1','$PID1','$WK1','$Name2','$PID2','$WK2')";
+            mysqli_query($conn, $sql);
+            $query = "SELECT Name1, WK1, Name2, WK2 FROM zapisani"; //You don't need a ; like you do in SQL
+            $result = mysqli_query($conn, $query);
+            echo "<table>"; // start a table tag in the HTML
+            while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+            echo "<tr><td>" . htmlspecialchars($row['Name1']) . "</td><td>" . htmlspecialchars($row['WK1']) . "</td><td>" . htmlspecialchars($row['Name2']) . "</td><td>" . htmlspecialchars($row['WK2']) . "</td></tr>";  //$row['index'] the index here is a field name
+            }
         
-        while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-        echo "<tr><td>" . htmlspecialchars($row['Name1']) . "</td><td>" . htmlspecialchars($row['WK1']) . "</td><td>" . htmlspecialchars($row['Name2']) . "</td><td>" . htmlspecialchars($row['WK2']) . "</td></tr>";  //$row['index'] the index here is a field name
-        }
+            echo "</table>"; //Close the table in HTML
+            }
+
         
-        echo "</table>"; //Close the table in HTML
         
         mysqli_close($conn);
         ?>
